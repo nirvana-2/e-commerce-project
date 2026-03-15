5bc03758c601b0db91bc35a2512962a967320ba4
saman shakya <samanshakya@samans-MacBook-Air.local>
2026-03-15 11:36:01 +0545

    update cors for vercel

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 // 2️⃣ Middleware & CORS
 // We include both localhost and 127.0.0.1 to prevent browser mismatches
 app.use(cors({
-    origin: ["http://localhost:5173", "https://nirvana-2.vercel.app"],
+    origin: ["http://localhost:5173", "https://e-commerce-project-eta-dun.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
